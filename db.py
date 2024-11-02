87cacbb1b13edadae6b21f4ec912d548f60bb4cc
Sergey13131313 <110328066+Sergey13131313@users.noreply.github.com>
2024-11-03 01:01:39 +0500

    Add files via upload

--- db.py
+++ db.py
@@ -3,16 +3,16 @@ import sqlite3
 connect = sqlite3.connect('not_telegram.db')
 curs = connect.cursor()
 
-create_db = ('CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY,'
-             'username TEXT NOT NULL,'
-             'email TEXT NOT NULL,'
-             'age INTEGER,'
-             'balance INTEGER NOT NULL)')
-curs.execute(create_db)
-
+# create_db = ('CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY,'
+#              'username TEXT NOT NULL,'
+#              'email TEXT NOT NULL,'
+#              'age INTEGER,'
+#              'balance INTEGER NOT NULL)')
+# curs.execute(create_db)
+#
 # for i in range(1, 11):
 #     curs.execute('INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)',
-#                  (f'User{i}', f'example{i}@gmail.com', i*10, i*1000))
+#                  (f'User{i}', f'example{i}@gmail.com', i*10, 1000))
 
 # curs.execute('SELECT id FROM Users')
 # b = True
@@ -26,11 +26,24 @@ curs.execute(create_db)
 # curs.execute('SELECT id FROM Users')
 # b = 1
 # for x in curs.fetchall():
-#     if b == 3:
+#     if b == 1:
 #         curs.execute('DELETE FROM Users WHERE id = ?', (x[0],))
-#         b = 1
+#         b = 3
 #     else:
-#         b += 1
+#         b -= 1
+
+# curs.execute('DELETE FROM Users WHERE id = ?', (6,))
+
+curs.execute('SELECT COUNT(*) FROM Users')
+print(curs.fetchone())
+
+
+curs.execute('SELECT SUM(balance) FROM Users')
+print(curs.fetchone())
+
+curs.execute('SELECT AVG(balance) FROM Users')
+print(curs.fetchone())
+
 
 curs.execute('SELECT * FROM Users WHERE age != ?', (50, ))
 
